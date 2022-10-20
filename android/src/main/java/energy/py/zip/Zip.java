94c54d0f777fdc1c34bd6eb505f4f896c5fecf12
Philipp Anné <philipp.anne@enertrag.com>
2022-10-20 17:04:33 +0200

    android implementation

--- android/src/main/java/energy/py/zip/Zip.java
+++ android/src/main/java/energy/py/zip/Zip.java
@@ -1,11 +1,82 @@
 package energy.py.zip;
 
-import android.util.Log;
+import android.content.Context;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public String echo(String value) {
-        Log.i("Echo", value);
-        return value;
+    private static final int BUFFER = 2048;
+
+    private void addFileToZip(File sourceFile, String asName, ZipOutputStream out) throws IOException {
+
+        byte data[] = new byte[BUFFER];
+
+        FileInputStream fi = new FileInputStream(sourceFile);
+        BufferedInputStream origin = new BufferedInputStream(fi, BUFFER);
+        ZipEntry entry = new ZipEntry(asName);
+        entry.setTime(sourceFile.lastModified()); // to keep modification time after unzipping
+        out.putNextEntry(entry);
+        int count;
+        while ((count = origin.read(data, 0, BUFFER)) != -1) {
+            out.write(data, 0, count);
+        }
+        origin.close();
     }
+
+
+    public File zipDirectory(Context context, String sourceDir, String destFilename) {
+
+        File sourceFile = new File(sourceDir);
+        try {
+            File outputDir = context.getCacheDir();
+            File outputFile = new File(outputDir.getPath() + File.separator + destFilename);
+
+            FileOutputStream dest = new FileOutputStream(outputFile);
+            ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(
+                    dest));
+
+            int baseLength;
+            if (sourceFile.isDirectory()) {
+                baseLength = sourceFile.getPath().length() + 1; // +slash
+            } else {
+                baseLength = sourceFile.getParent().length();
+            }
+
+            zipRecursive(out, sourceFile, baseLength);
+
+            out.close();
+
+            return outputFile;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void zipRecursive(ZipOutputStream out, File file,
+                              int basePathLength) throws IOException {
+
+        if (file.isDirectory()) {
+            File[] fileList = file.listFiles();
+            for (File child : fileList) {
+                zipRecursive(out, child, basePathLength);
+            }
+        } else {
+            String unmodifiedFilePath = file.getPath();
+            String relativePath = unmodifiedFilePath
+                    .substring(basePathLength);
+
+            addFileToZip(file, relativePath, out);
+        }
+    }
+
 }
--- android/src/main/java/energy/py/zip/ZipPlugin.java
+++ android/src/main/java/energy/py/zip/ZipPlugin.java
@@ -1,10 +1,18 @@
 package energy.py.zip;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+
+import java.io.File;
+
+import androidx.core.content.FileProvider;
 
 @CapacitorPlugin(name = "Zip")
 public class ZipPlugin extends Plugin {
@@ -14,13 +22,44 @@ public class ZipPlugin extends Plugin {
     @PluginMethod
     public void isAvailable(PluginCall call) {
         JSObject ret = new JSObject();
-        ret.put("available", false);
+        ret.put("available", true);
 
         call.resolve(ret);
     }
 
     @PluginMethod
     public void shareArchive(PluginCall call) {
-        call.reject("not yet implemented");
+
+        String sourceDir = call.getString("sourceDir");
+        String destFilename = call.getString("destFilename");
+
+        if(sourceDir.toLowerCase().startsWith("file://")) {
+            sourceDir = sourceDir.substring(7);
+        }
+
+        Context context = this.getContext();
+        JSObject result = new JSObject();
+
+        File file = implementation.zipDirectory(context, sourceDir, destFilename);
+        if (file != null) {
+
+            String packageName = this.getActivity().getApplicationContext().getPackageName();
+
+            Uri uri = FileProvider.getUriForFile(getActivity(), packageName + ".fileprovider", file);
+
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+            shareIntent.setType("application/octet-stream");
+
+            this.getActivity().startActivity(Intent.createChooser(shareIntent, null));
+
+            result.put("success", true);
+
+        } else {
+            result.put("success", false);
+        }
+
+        call.resolve(result);
     }
 }
